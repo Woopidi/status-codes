3451d11a3f9dd3c66f7cf26165ba3bb695198302
Altus Carstens <carstensaltus@gmail.com>
2017-12-20 23:12:55 +0200

    Adding documentation

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,23 @@
 export enum Codes {
-  OK = 200,
-  Created = 201,
-  Accepted = 202,
-  NonAuthoritativeInformation = 203,
-  NoContent = 204,
-  ResetContent = 205,
-  PartialContent = 206,
-  MultiStatus = 207,
-  AlreadyReported = 208,
-  IMUsed = 226
+
+	/**
+	* statusCode: 200
+	* 
+	* Standard response for successful HTTP requests. 
+	* The actual response will depend on the request method used. 
+	* In a GET request, the response will contain an entity corresponding to the 
+	*/
+	OK = 200,
+
+	Created = 201,
+	Accepted = 202,
+	NonAuthoritativeInformation = 203,
+	NoContent = 204,
+	ResetContent = 205,
+	PartialContent = 206,
+	MultiStatus = 207,
+	AlreadyReported = 208,
+	IMUsed = 226
 }
 
 export default Codes;
