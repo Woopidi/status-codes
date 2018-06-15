0cbdd0c1d4891768351e6d785c180d8314a1db4c
Altus Carstens <carstensaltus@gmail.com>
2018-06-15 12:42:08 +0200

    fix(pencil): stop graphite breaking when too much pressure applied

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ export { Information } from "./information";
 export { Success } from "./success";
 export { ClientError } from "./client-error";
 export { ServerError } from "./server-error";
-// Testing
+// Testing .dfdkj
