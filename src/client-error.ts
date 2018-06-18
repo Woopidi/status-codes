d2e1ce9b4ab6eb6a76b92dda116ad2c0f5d949c1
Altus Carstens <carstensaltus@gmail.com>
2018-06-18 10:47:12 +0200

    fix(pencil): Rewriting some stuff

--- src/client-error.ts
+++
@@ -1,9 +0,0 @@
-export enum ClientError {
-	BadRequest = 400,
-	Unauthorized = 401,
-	PaymentRequired = 402,
-	Forbidden = 403,
-	NotFound = 404
-}
-
-export default ClientError;
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,55 @@
-export { Information } from "./information";
-export { Success } from "./success";
-export { ClientError } from "./client-error";
-export { ServerError } from "./server-error";
-// Testing .dfdkj
 
 
-export const Ok = 200;
+export namespace Types {
 
-export const Oops = 300;
+    // /**
+    //  * Standard response for successful HTTP requests. The actual response will depend on the request method used. In a GET request, the response will contain an entity corresponding to the requested resource. In a POST request, the response will contain an entity describing or containing the result of the action.
+    //  */
+    // export type OK = 200;
+    // /**
+    //  * The request has been fulfilled, resulting in the creation of a new resource.
+    //  */
+    // export type Created = 201;
+    /**
+     * An informational response indicates that the request was received and understood. It is issued on a provisional basis while request processing continues. It alerts the client to wait for a final response. The message consists only of the status line and optional header fields, and is terminated by an empty line. As the HTTP/1.0 standard did not define any 1xx status codes, servers must not send a 1xx response to an HTTP/1.0 compliant client except under experimental conditions.
+     */
+    export type Information = 100|101|102|103
+    
+    /**
+     * This class of status codes indicates the action requested by the client was received, understood and accepted.
+     */
+    export type Success = 200|201;
+}
+
+export const enum HttpStatus {
+    /**
+     * Used to return some response headers before final HTTP message.
+     */
+    CONTINUE = 100,
+    /**
+     * The requester has asked the server to switch protocols and the server has agreed to do so.
+     */
+    SWITCH_PROTOCOLS = 101,
+    /**
+     * A WebDAV request may contain many sub-requests involving file operations, requiring a long time to complete the request. This code indicates that the server has received and is processing the request, but no response is available yet. This prevents the client from timing out and assuming the request was lost.
+     */
+    PROCESSING = 102,
+    /**
+     * Used to return some response headers before final HTTP message.
+     */
+    EARLY_HINTS = 103,
+    /**
+     * Standard response for successful HTTP requests. The actual response will depend on the request method used. In a GET request, the response will contain an entity corresponding to the requested resource. In a POST request, the response will contain an entity describing or containing the result of the action.
+     */
+    OK = 200,
+    /**
+     * The request has been fulfilled, resulting in the creation of a new resource.
+     */
+    CREATED = 201,
+    /**
+     * The request has been accepted for processing, but the processing has not been completed. The request might or might not be eventually acted upon, and may be disallowed when processing occurs.
+     */
+    ACCEPTED = 202,
+}
+
+export default HttpStatus;
